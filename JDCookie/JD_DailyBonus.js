9fae3d98afaadb581d1671f238274791612233ca
kettle798 <72073717+kettle798@users.noreply.github.com>
2021-08-28 15:30:11 +0800

    Update JD_DailyBonus.js

--- JDCookie/JD_DailyBonus.js
+++ JDCookie/JD_DailyBonus.js
@@ -48,7 +48,8 @@ var DualKey = ''; //该参数已废弃; 仅用于下游脚本的兼容, 请使�
 
 var OtherKey = `[{
   "cookie": "pt_key=AAJhKbPdADDDnAPyiIuy7DQF2jEn7mWUmrWr1NBAhUys3MYU6VfMz4LyGrXYCuycH9bdOG3pbbQ; pt_pin=chuixue;"
-}]`; //无限账号Cookie json串数据, 请严格按照json格式填写, 具体格式请看以下样例:
+}
+]`; //无限账号Cookie json串数据, 请严格按照json格式填写, 具体格式请看以下样例:
 
 /*以下样例为双账号("cookie"为必须,其他可选), 第一个账号仅包含Cookie, 第二个账号包含Cookie和金融签到Body: 
 
